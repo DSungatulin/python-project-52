2ed3dff1c58adcd0be716fe01cf648804f04faaa
DSungatulin <sungatulindaniyar@gmail.com>
2024-08-03 01:35:55 +0600

    edit statuses view

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -40,7 +40,7 @@ class StatusDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Status
     login_url = reverse_lazy('/login/')
     success_message = _('Status successfully deleted')
-    success_url = reverse_lazy('/statuses/')
+    success_url = reverse_lazy('statuses-detail')
     template_name = 'statuses/delete.html'
     extra_context = {
         'title': _('Delete Status'),
