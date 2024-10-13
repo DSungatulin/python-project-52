6854a601a8a56144b1de0b7a0f47ea11c31a081d
DSungatulin <sungatulindaniyar@gmail.com>
2024-10-13 12:11:12 +0600

    fix action issues

--- task_manager/task/views.py
+++ task_manager/task/views.py
@@ -37,8 +37,8 @@ class UpdateTask(
 
 
 class DeleteTask(
-    LoginRequiredMixinWithFlash,
     UserChangeOwnDataMixin,
+    LoginRequiredMixinWithFlash,
     SuccessMessageMixin,
     DeleteView
 ):
