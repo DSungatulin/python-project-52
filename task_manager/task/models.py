dcf3b34cb3eaeaa9b55ae30443ae0888704b3763
DSungatulin <sungatulindaniyar@gmail.com>
2024-10-19 22:51:38 +0600

    edit Task model

--- task_manager/task/models.py
+++ task_manager/task/models.py
@@ -1,44 +1,43 @@
 from django.db import models
-from django.utils import timezone
-from task_manager.status.models import Status
 from task_manager.user.models import User
+from task_manager.status.models import Status
 from task_manager.label.models import Label
+from django.utils.translation import gettext_lazy as _
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=40, unique=True)
-    description = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=200,
+                            unique=True,
+                            verbose_name=_('Name'))
+    description = models.TextField(blank=True, verbose_name=_('Description'))
     status = models.ForeignKey(
         Status,
-        on_delete=models.PROTECT,
-        null=False,
-        blank=False,
         related_name='tasks',
-    )
+        on_delete=models.PROTECT,
+        verbose_name=_('Status'))
     executor = models.ForeignKey(
         User,
-        on_delete=models.PROTECT,
+        related_name='assigned_tasks',
         null=True,
         blank=True,
-        related_name='assigned_tasks',
-    )
+        on_delete=models.PROTECT,
+        verbose_name=_('Executor'))
     labels = models.ManyToManyField(
         Label,
+        related_name='task_labels',
         blank=True,
-        related_name='task_labels'
-    )
+        verbose_name=_('Labels'))
     author = models.ForeignKey(
         User,
+        related_name='created_tasks',
         on_delete=models.PROTECT,
-        null=False,
-        blank=False,
-        related_name='created_tasks'
-    )
-    created_at = models.DateTimeField(default=timezone.now)
+        verbose_name=_('Author'))
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['id']
+        verbose_name = _('Task')
 
     def __str__(self):
         return self.name
